7e6d5b0dc3cba9950a7335b541831a3c8f560b05
KimTaeyoun <kimty1121@gmail.com>
2021-07-30 21:45:42 +0900

    test about types

---
+++ types.js
@@ -0,0 +1,18 @@
+// TypeScript
+// type 변경 불가
+var x = 10;
+// x = "hello";
+/*
+number
+string
+boolean
+any
+*/
+var i;
+var i = 10;
+var i = 10; // i: number
+var y; // var y: any
+y = 10;
+y = "hello";
+var result;
+result = [10, 5, 3];
---
+++ types.ts
@@ -0,0 +1,23 @@
+
+// TypeScript
+// type 변경 불가
+var x: number = 10;
+// x = "hello";
+
+/*
+number
+string
+boolean
+any
+*/
+
+var i: number;
+var i: number = 10;
+var i = 10; // i: number
+
+var y; // var y: any
+y = 10;
+y = "hello";
+
+var result: number[];
+result = [10, 5, 3];
