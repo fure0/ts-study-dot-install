463d9e1711fa4fcf5de4ad968dd55be97196ae4a
KimTaeyoun <kimty1121@gmail.com>
2021-07-31 21:13:04 +0900

    enum type

---
+++ types2.ts
@@ -0,0 +1,30 @@
+//열거형
+
+/*
+enum Signal {
+    Red = 0,
+    Blue = 1,
+    Yellow = 2
+}
+*/
+
+enum Signal {
+    Red, //0
+    Blue = 3,
+    Yellow //4
+}
+
+enum Signal {
+    Green = 5
+}
+
+var result2: Signal;
+
+// if (result === Signal.Yellow) { ... }
+// if (result === Signal['Yellow']) { ... }
+
+//console.log(Signal[2]); // Yellow
+
+console.log(Signal[3]); // Blue
+
+console.log(Signal.Green); // 5
